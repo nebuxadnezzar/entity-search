ba1a9bfa8ae86ece162fa5543bcdba57b3f5fe63
nebuxadnezzar <nebuxadnezzar@aol.com>
2026-05-14 12:29:46 -0400

    added h2 stored proc module

---
+++ src/main/java/com/entity/tools/H2Functions.java
@@ -0,0 +1,11 @@
+package com.entity.tools;
+
+//import java.sql.*;
+//import java.util.*;
+
+public class H2Functions {
+
+    public static String toString(Object o) {
+        return o != null ? o.toString() : "";
+    }
+}
